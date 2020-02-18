a59319d25bba0f9a49b760f482492b66d990a9de
CaelRo <alex@tsybulin.ru>
2020-02-18 19:07:22 +0400

    add style, make first page (test)

--- js/test-table.js
+++ js/test-table.js
@@ -1,12 +1,12 @@
-for (var i=0; i<6; i++) {
-    var row = document.querySelector("table").insertRow(-1);
-    for (var j=0; j<6; j++) {
-        var letter = String.fromCharCode("A".charCodeAt(0)+j-1);
+for (let i=0; i<7; i++) {
+    const row = document.querySelector("table").insertRow(-1);
+    for (let j=0; j<360; j++) {
+        let letter = String.fromCharCode("A".charCodeAt(0)+j-1);
         row.insertCell(-1).innerHTML = i&&j ? "<input id='"+ letter+i +"'/>" : i||letter;
     }
 }
 
-var DATA={}, INPUTS=[].slice.call(document.querySelectorAll("input"));
+const DATA={}, INPUTS=[].slice.call(document.querySelectorAll("input"));
 INPUTS.forEach(function(elm) {
     elm.onfocus = function(e) {
         e.target.value = localStorage[e.target.id] || "";
@@ -15,8 +15,8 @@ INPUTS.forEach(function(elm) {
         localStorage[e.target.id] = e.target.value;
         computeAll();
     };
-    var getter = function() {
-        var value = localStorage[elm.id] || "";
+    let getter = function() {
+        let value = localStorage[elm.id] || "";
         if (value.charAt(0) === "=") {
             with (DATA) return eval(value.substring(1));
         } else {
--- templates/main.tpl
+++ templates/main.tpl
@@ -1,14 +1,23 @@
-<html lang="ru">
+<html>
 <head>
     <title>{TITLE}</title>
+    <link type="text/css" rel="stylesheet" href="../css/style.css" />
     <link type="text/css" rel="stylesheet" href="../css/test-table.css" />
 </head>
 <body style="background-color: {BGCOLOR};">
 
-{SOMETPLTAGS}
+<div class="wrapper">
+  <div class="header">header</div>
+  <div class="content">
+    <div class="navigate">{SOMETPLTAGS}</div>
+    <div class="main">
+      <table></table>
+    </div>
+  </div>
+  <div class="footer">footer</div>
+</div>
 
 <script type="text/javascript" src="../js/test-table.js"></script>
-<table></table>
 
 </body>
 </html>
