7977890d8d7b4d9bbb1041c791950d373593a475
alex <alex@tsybulin.ru>
2021-11-30 17:36:27 +0300

    начал функцию добавления сотрудника в классе сотрудников

--- classes/peoples.php
+++ classes/peoples.php
@@ -11,18 +11,22 @@ class peoples {
     $this->db = new sqlite("db.sqlite");
   }
 
-  function get_people($id) {
-    $this->man = $this->db->queryRow("select * from peoples where peopleid=".$id);
-    return $this->man;
-  }
-
   function get_list_peoples() {
     $this->people_list = $this->db->queryAll("select * from peoples");
     return $this->people_list;
   }
 
+  function get_man($id) {
+    $this->man = $this->db->queryRow("select * from peoples where peopleid=".$id);
+    return $this->man;
+  }
+
   function add_man($data) {
-    // $this->db->query(sprintf("insert into peoples values ()"))
+    $this->db->query(sprintf("insert into peoples ('lname','fname','mname') values ('%s','%s','%s')",$this->db->clean(),$this->db->clean(),$this->db->clean()));
+    $this->db->query(sprintf("insert into peoples ('birthday') values ()",));
+    $this->db->query(sprintf("insert into peoples ('sexid') values ()",));
+    $this->db->query(sprintf("insert into peoples ('departmentid','positionid') values ()",));
+    $this->db->query(sprintf("insert into peoples ('tab_N') values ()",));
   }
 
   function update_man($id, $data) {
